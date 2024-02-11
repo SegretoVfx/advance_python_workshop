7252b281b9a9b7c6ad61a15f08ee19f309d07050
segretovfx <segretovfx@gmail.com>
2024-02-11 16:38:28 -0500

    DONE 02_style

--- 02_style/22_pylogger.py
+++ 02_style/22_pylogger.py
@@ -3,29 +3,31 @@
 #
 # date    = 2022-01-07
 # email   = contact@alexanderrichtertd.com
-#************************************************************************************
+# ************************************************************************************
 
-# original: logging.init.py
 
-def findCaller(self):
+def find_caller_stack_frame(self):
     """
-    Find the stack frame of the caller so that we can note the source
-    file name, line number and function name.
+    Find the stack frame of the caller
+
+    Returns:
+        rv (str, int, str) : File name, line number, Function name
     """
-    f = currentframe()
-    #On some versions of IronPython, currentframe() returns None if
-    #IronPython isn't run with -X:Frames.
-    if f is not None:
-        f = f.f_back
-    rv = "(unknown file)", 0, "(unknown function)"
-    while hasattr(f, "f_code"):
-        co = f.f_code
-        filename = os.path.normcase(co.co_filename)
-        if filename == _srcfile:
-            f = f.f_back
+    cur_frame = currentframe()
+
+    # if IronPython isn't run with '-X:Frames' : currentframe() returns None.
+    if cur_frame is not None:
+        cur_frame = cur_frame.f_back
+
+    while hasattr(cur_frame, "f_code"):
+
+        frame_code = cur_frame.f_code
+        file_name = os.path.normcase(frame_code.co_filename)
+
+        if file_name == _srcfile:
+            cur_frame = cur_frame.f_back
             continue
-        rv = (co.co_filename, f.f_lineno, co.co_name)
-        break
-    return rv
 
-# How can we make this code better?
+        return frame_code.co_filename, cur_frame.f_lineno, frame_code.co_name
+
+    return "(unknown file)", 0, "(unknown function)"
